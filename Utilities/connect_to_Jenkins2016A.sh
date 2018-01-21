71fd07c3e47c31140aed68d9cba05d228dcc1492
Peter Stadler <stadlerpeter@yahoo.fr>
2018-01-21 23:50:13 +0100

    removed outdated scripts

--- Utilities/connect_to_Jenkins2016A.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Connecting to the Virtual Machine called Jenkins2016A"
-echo "Sending Firefox to http://localhost:7070/"
-firefox "http://localhost:7070/"
-echo "Now ssh-ing into the machine."
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l hcmc -p 2016 localhost
